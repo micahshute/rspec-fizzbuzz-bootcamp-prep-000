dbaf1e076e272e269c0375d44ac7cc5c03d2ce29
Micah Shute <micah.shute@gmail.com>
2018-02-02 03:07:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,7 @@ def fizzbuzz(number)
   answer = ""
   divByThree = ((number % 3) == 0)
   divByFive = (number % 5) == 0) 
-  if (divByThree) == 0
-    answer += "Fizz"
-  else
-    divByThree = FALSE
-  end
-  if (divByFive) == 0 
-    answer += "Buzz"
-  else
-    divByFive = FALSE
-  end
+  
   return (!divByThree && !divByFive) ? NIL : answer
+  
 end
